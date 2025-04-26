7ab379a1390df50a3bf71a81c87292d41f89903a
josh <infinityjosh.jn@gmail.com>
2025-04-26 08:08:07 +0300

    integrated stacked

---
+++ test/viewmodels/home_viewmodel_test.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:health_managment_system/app/app.bottomsheets.dart';
+import 'package:health_managment_system/app/app.locator.dart';
+import 'package:health_managment_system/ui/common/app_strings.dart';
+import 'package:health_managment_system/ui/views/home/home_viewmodel.dart';
+
+import '../helpers/test_helpers.dart';
+
+void main() {
+  HomeViewModel getModel() => HomeViewModel();
+
+  group('HomeViewmodelTest -', () {
+    setUp(() => registerServices());
+    tearDown(() => locator.reset());
+
+    group('incrementCounter -', () {
+      test('When called once should return  Counter is: 1', () {
+        final model = getModel();
+        model.incrementCounter();
+        expect(model.counterLabel, 'Counter is: 1');
+      });
+    });
+
+    group('showBottomSheet -', () {
+      test('When called, should show custom bottom sheet using notice variant', () {
+        final bottomSheetService = getAndRegisterBottomSheetService();
+
+        final model = getModel();
+        model.showBottomSheet();
+        verify(bottomSheetService.showCustomSheet(
+          variant: BottomSheetType.notice,
+          title: ksHomeBottomSheetTitle,
+          description: ksHomeBottomSheetDescription,
+        ));
+      });
+    });
+  });
+}
