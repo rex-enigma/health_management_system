b512895d012fb6c3d40e7d3772945e9d4e09658c
josh <infinityjosh.jn@gmail.com>
2025-04-27 12:14:52 +0300

    created reusable input text field

---
+++ lib/ui/reusable_widgets/app_text_field.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AppTextField extends StatelessWidget {
+  final String label;
+  final TextEditingController controller;
+  final TextInputType? keyboardType;
+  final bool obscureText;
+
+  const AppTextField({
+    Key? key,
+    required this.label,
+    required this.controller,
+    this.keyboardType,
+    this.obscureText = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        labelText: label,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        contentPadding:
+            const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      ),
+    );
+  }
+}
