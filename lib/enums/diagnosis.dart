6ddb1bf167ac29f1d16aea1cfb8aa01b8e06ff6a
josh <infinityjosh.jn@gmail.com>
2025-04-27 23:31:37 +0300

    included some diagnosis with their conversion to Diagnosis enum

--- lib/enums/diagnosis.dart
+++ lib/enums/diagnosis.dart
@@ -1,11 +1,24 @@
 enum Diagnosis {
   hivPositive,
+  tb,
+  malaria,
+  asthma,
+  covid19,
+  diabetesType2,
   none;
 
   static Diagnosis fromString(String value) {
-    switch (value) {
-      case 'hivPositive':
-        return Diagnosis.hivPositive;
+    switch (value.toLowerCase()) {
+      case 'malaria':
+        return Diagnosis.malaria;
+      case 'asthma':
+        return Diagnosis.asthma;
+      case 'covid19':
+        return Diagnosis.covid19;
+      case 'diabetesType2':
+        return Diagnosis.diabetesType2;
+      case 'none':
+        return Diagnosis.none;
       default:
         throw "the string: $value, doesn't correspond to any diagnosis constant value";
     }
