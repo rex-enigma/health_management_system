a2823aa838ebd358a29b6b73bc41ba6eb8a280ac
josh <infinityjosh.jn@gmail.com>
2025-04-27 12:14:09 +0300

    implemented info dialog

--- lib/ui/dialogs/info_alert/info_alert_dialog_model.dart
+++ lib/ui/dialogs/info_alert/info_alert_dialog_model.dart
@@ -1,3 +1,16 @@
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-class InfoAlertDialogModel extends BaseViewModel {}
+class InfoAlertDialogModel extends BaseViewModel {
+  final Function(DialogResponse) completer;
+
+  InfoAlertDialogModel(this.completer);
+
+  void onCancel() {
+    completer(DialogResponse(confirmed: false));
+  }
+
+  void onConfirm() {
+    completer(DialogResponse(confirmed: true));
+  }
+}
