91bd225de773c237fb8db83afeeacc3d219739f9
josh <infinityjosh.jn@gmail.com>
2025-05-08 21:56:11 +0300

    - added `DiagnosisSelectionViewModel` mixin to add 2 methods and  1 property to `RegisterClientViewModel`: 1. `openDiagnosisSelection` method for opening diagnosis selection bottomsheet 2.  `removeDiagnosis` for removing the current selected diagnosis. 3.  `selectedDiagnoses` for holding selected diagnoses. - modified `_selectedDiagnoses` to hold set type to handle duplicate. - modified `CreateClientParams` to use diagnosisIds instead of diagnosisNames - modified the navigation back to return the newly registered client

--- lib/ui/views/register_client/register_client_viewmodel.dart
+++ lib/ui/views/register_client/register_client_viewmodel.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:health_managment_system/app/app.bottomsheets.dart';
 import 'package:health_managment_system/app/app.dialogs.dart';
 import 'package:health_managment_system/app/app.locator.dart';
+import 'package:health_managment_system/domain/entities/diagnosis_entity.dart';
 import 'package:health_managment_system/domain/usecases/create_client_usecase.dart';
-import 'package:health_managment_system/ui/dialogs/info_alert/info_alert_dialog.dart';
+import 'package:health_managment_system/enums/diagnosis_selection_mode.dart';
+import 'package:health_managment_system/ui/reusable_widgets/select_diagnoses_widget.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 import '../../../enums/gender.dart';
-import '../../../enums/diagnosis.dart';
 
-class RegisterClientViewModel extends BaseViewModel {
+class RegisterClientViewModel extends BaseViewModel with DiagnosisSelectionViewModel {
   final _navigationService = locator<NavigationService>();
   final _dialogService = locator<DialogService>();
+  final _bottomSheetService = locator<BottomSheetService>();
   final _createClientUseCase = locator<CreateClientUseCase>();
 
   final firstNameController = TextEditingController();
@@ -18,14 +21,15 @@ class RegisterClientViewModel extends BaseViewModel {
   final contactInfoController = TextEditingController();
   final addressController = TextEditingController();
 
-  Gender? _selectedGender = Gender.other;
+  Gender? _selectedGender;
   Gender? get selectedGender => _selectedGender;
 
   DateTime? _dateOfBirth;
   DateTime? get dateOfBirth => _dateOfBirth;
 
-  List<Diagnosis> _selectedDiagnoses = [];
-  List<Diagnosis> get selectedDiagnoses => _selectedDiagnoses;
+  Set<DiagnosisEntity> _selectedDiagnoses = {};
+  @override
+  Set<DiagnosisEntity> get selectedDiagnoses => _selectedDiagnoses;
 
   void setSelectedGender(Gender? gender) {
     _selectedGender = gender;
@@ -45,19 +49,28 @@ class RegisterClientViewModel extends BaseViewModel {
     }
   }
 
-  void toggleDiagnosis(Diagnosis diagnosis) {
-    if (_selectedDiagnoses.contains(diagnosis)) {
-      _selectedDiagnoses.remove(diagnosis);
-    } else {
-      _selectedDiagnoses.add(diagnosis);
-    }
+  @override
+  Future<void> openDiagnosisSelection() async {
+    final result = await _bottomSheetService.showCustomSheet(
+      variant: BottomSheetType.diagnosisSelection,
+      ignoreSafeArea: false,
+      isScrollControlled: true,
+      // should always be in that order
+      data: (_selectedDiagnoses, DiagnosisSelectionMode.multiple),
+    );
+
+    _selectedDiagnoses = result?.data ?? {};
+    notifyListeners();
+  }
+
+  @override
+  void removeDiagnosis(DiagnosisEntity diagnosis) {
+    _selectedDiagnoses.remove(diagnosis);
     notifyListeners();
   }
 
   Future<void> registerClient() async {
-    if (firstNameController.text.isEmpty ||
-        lastNameController.text.isEmpty ||
-        contactInfoController.text.isEmpty) {
+    if (firstNameController.text.isEmpty || lastNameController.text.isEmpty || contactInfoController.text.isEmpty) {
       _dialogService.showCustomDialog(
         variant: DialogType.infoAlert,
         title: 'Error',
@@ -75,16 +88,26 @@ class RegisterClientViewModel extends BaseViewModel {
       return;
     }
 
+    if (_selectedGender == null) {
+      _dialogService.showCustomDialog(
+        variant: DialogType.infoAlert,
+        title: 'Error',
+        description: 'Gender is required.',
+      );
+      return;
+    }
+
     final client = CreateClientParams(
       firstName: firstNameController.text,
       lastName: lastNameController.text,
-      gender: _selectedGender?.name ?? Gender.other.name,
+      gender: _selectedGender!.name,
       dateOfBirth: _dateOfBirth!.toIso8601String(),
       contactInfo: contactInfoController.text,
       address: addressController.text,
-      diagnosisNames: _selectedDiagnoses.map((diagnosis) => diagnosis.name).toList(),
+      diagnosisIds: _selectedDiagnoses.map((diagnosis) => diagnosis.id).toList(),
     );
 
+    setBusy(true);
     final result = await _createClientUseCase(client);
 
     result.fold(
@@ -95,10 +118,11 @@ class RegisterClientViewModel extends BaseViewModel {
           description: 'Failed to register client: ${failure.message}',
         );
       },
-      (_) {
-        _navigationService.back();
+      (client) {
+        _navigationService.back(result: client.id);
       },
     );
+    setBusy(false); // will also call notifyListener
   }
 
   @override
